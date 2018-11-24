304ac4e155d379afd81f341cb05ef6e43738978b
Daniel Fries <daniel.w.fries@gmail.com>
2018-11-24 21:32:07 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,11 +3,10 @@
 
 def fizzbuzz(int)
   result = ""
-  if int % 15 == 0
-    result = result + "FizzBuzz"
-  elsif int % 5 == 0
+  if int % 3 == 0
+    result = result + "Fizz"
+  end
+  if int % 5 == 0
     result = result + "Buzz"
-  elsif int % 3 == 0
-    result = result "Fizz"
   end
 end
